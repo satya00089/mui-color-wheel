7a1dcce4ad773278a9416beab66463807a9edb10
SubudhiK <ksatya.subudhi@unisys.com>
2025-08-30 15:37:21 +0530

    dist removed

--- dist/utils/colorConverters.d.ts
+++
@@ -1,8 +0,0 @@
-import { RGB, HSL } from "../types";
-export declare const colorConverters: {
-    hexToRgb(hex: string): RGB | null;
-    rgbToHex(r: number, g: number, b: number): string;
-    rgbToHsl(r: number, g: number, b: number): HSL;
-    hslToRgb(h: number, s: number, l: number): RGB;
-};
-//# sourceMappingURL=colorConverters.d.ts.map
